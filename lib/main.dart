d737aa025ce7ea4f93cba960626c22606996f5fc
Guilherme Estimo <guilherme.estimo@hotmail.com>
2021-06-10 15:24:28 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:vamos_cozinhar/data/dummy_data.dart';
+import 'package:vamos_cozinhar/models/settings.dart';
+import 'package:vamos_cozinhar/screens/categories_meals_screen.dart';
+import 'package:vamos_cozinhar/screens/meal_detail_screen.dart';
+import 'package:vamos_cozinhar/screens/settings_screen.dart';
+import 'package:vamos_cozinhar/screens/tabs_screen.dart';
+import 'package:vamos_cozinhar/utils/app-routes.dart';
+
+import 'models/meal.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Settings settings = Settings();
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
+
+  void _filterMeals(Settings settings) {
+    setState(() {
+      this.settings = settings;
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = settings.isVegan && !meal.isVegan;
+        final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
+
+        return !filterGluten &&
+            !filterLactose &&
+            !filterVegan &&
+            !filterVegetarian;
+      }).toList();
+    });
+  }
+
+  void _toogleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal)
+          ? _favoriteMeals.remove(meal)
+          : _favoriteMeals.add(meal);
+    });
+  }
+
+  bool _isFavorite(Meal meal) {
+    return _favoriteMeals.contains(meal);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+      ),
+      routes: {
+        AppRoutes.HOME: (_) => TabsScreen(_favoriteMeals),
+        AppRoutes.CATEGORIES_MEALS: (_) =>
+            CategoriesMealsScreen(_availableMeals),
+        AppRoutes.MEAL_DETAIL: (_) =>
+            MealDetailScreen(_toogleFavorite, _isFavorite),
+        AppRoutes.SETTINGS: (_) => SettingsScreen(_filterMeals, settings),
+      },
+    );
+  }
+}
---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:vamos_cozinhar/components/category_item.dart';
+import 'package:vamos_cozinhar/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: DUMMY_CATEGORIES.map((categoria) {
+        return CategoryItem(categoria);
+      }).toList(),
+    );
+  }
+}
